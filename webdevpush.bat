98d978027b3b50346e2629f49347504365ec276b
Kushal <kushalgraju@gmail.com>
2025-08-06 22:18:35 +0530

    final

--- webdevpush.bat
+++ webdevpush.bat
@@ -4,12 +4,16 @@ cd /d "%~dp0"
 :: Ask for today's file name
 set /p filename=Enter today's filename (without extension): 
 
-:: Copy main.html to <your filename>.html
+:: Copy main.html to DayX.html
 copy main.html %filename%.html
 
 :: Git commands
-git add .
-git commit -am "Added %filename%.html"
+git add %filename%.html
+git commit -m "Added %filename%.html"
+git pull origin master --rebase
 git push origin master
+
+:: Delete the copied file locally
+del %filename%.html
 
 pause
